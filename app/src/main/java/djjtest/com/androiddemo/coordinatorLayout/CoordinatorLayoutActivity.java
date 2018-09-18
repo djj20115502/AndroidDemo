7f7d06159fe35c51cf146f5efda0c49db8beb2ae
dongjunjie <dongjunjie.mail@qq.com>
2018-09-18 21:42:01 +0800

    添加基本布局

---
+++ app/src/main/java/djjtest/com/androiddemo/coordinatorLayout/CoordinatorLayoutActivity.java
@@ -0,0 +1,15 @@
+package djjtest.com.androiddemo.coordinatorLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import djjtest.com.androiddemo.R;
+
+public class CoordinatorLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coordinatorlayout_activity);
+    }
+}
