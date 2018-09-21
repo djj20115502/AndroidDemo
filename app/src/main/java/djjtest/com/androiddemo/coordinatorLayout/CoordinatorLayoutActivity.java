59d528a681248a92546131e2ccc78edc6702aa15
dongjunjie <361170803@qq.com>
2018-09-21 11:48:04 +0800

    基本的coordinate布局

--- app/src/main/java/djjtest/com/androiddemo/coordinatorLayout/CoordinatorLayoutActivity.java
+++ app/src/main/java/djjtest/com/androiddemo/coordinatorLayout/CoordinatorLayoutActivity.java
@@ -3,13 +3,40 @@ package djjtest.com.androiddemo.coordinatorLayout;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import djjtest.com.androiddemo.R;
 
 public class CoordinatorLayoutActivity extends Activity {
+    @BindView(R.id.coordinatorLayout_activity_collapsingToolbarLayout)
+    CollapsingToolbarLayout toolbarLayout;
+
+    @BindView(R.id.coordinatorLayout_activity_appBarLayout)
+    AppBarLayout appBarLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coordinatorlayout_activity);
+        ButterKnife.bind(this);
+        initView();
     }
+
+    private void initView() {
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                    toolbarLayout.setContentScrimColor(0xfff000);
+                } else {
+                    toolbarLayout.setContentScrim(null);
+                }
+            }
+        });
+    }
+
+
 }
