37d7390beb1199d5bd39a9595ca34e6ac818eaeb
djj <dongjunjie.mail@qq.com>
2019-12-24 10:33:16 +0800

    feat(rv):用submodule来处理flutter引用

---
+++ app/src/main/java/djjtest/com/androiddemo/flutter/FlutterFragment.java
@@ -0,0 +1,17 @@
+package djjtest.com.androiddemo.flutter;
+
+import djjtest.com.androiddemo.R;
+import djjtest.com.androiddemo.base.BaseDialogFragment;
+import djjtest.com.androiddemo.databinding.FragmentFlutterBinding;
+
+public class FlutterFragment extends BaseDialogFragment<FragmentFlutterBinding> {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_flutter;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
