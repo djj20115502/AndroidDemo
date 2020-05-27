31b8d73d020c7352a7d73cd6ae6292af4fb4674b
dongjunjie <dongjunjie@wegames.biz>
2020-05-27 16:14:48 +0800

    feat(去除butterknife): 准备接入firebase，去除butterknife

--- app/src/main/java/djjtest/com/androiddemo/coordinatorLayout/CoordinatorLayoutActivity.java
+++ app/src/main/java/djjtest/com/androiddemo/coordinatorLayout/CoordinatorLayoutActivity.java
@@ -1,38 +1,36 @@
 package djjtest.com.androiddemo.coordinatorLayout;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import djjtest.com.androiddemo.R;
+import djjtest.com.androiddemo.databinding.CoordinatorlayoutActivityBinding;
 
 public class CoordinatorLayoutActivity extends Activity {
-    @BindView(R.id.coordinatorLayout_activity_collapsingToolbarLayout)
-    CollapsingToolbarLayout toolbarLayout;
 
-    @BindView(R.id.coordinatorLayout_activity_appBarLayout)
-    AppBarLayout appBarLayout;
+    CoordinatorlayoutActivityBinding binding;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coordinatorlayout_activity);
-        ButterKnife.bind(this);
+        binding = DataBindingUtil.setContentView(this, R.layout.coordinatorlayout_activity);
+
         initView();
     }
 
     private void initView() {
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+        binding.coordinatorLayoutActivityAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                 if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
-                    toolbarLayout.setContentScrimColor(0xfff000);
+                    binding.coordinatorLayoutActivityCollapsingToolbarLayout.setContentScrimColor(0xfff000);
                 } else {
-                    toolbarLayout.setContentScrim(null);
+                    binding.coordinatorLayoutActivityCollapsingToolbarLayout.setContentScrim(null);
                 }
             }
         });
